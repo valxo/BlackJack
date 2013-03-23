0d058d15dc35afeb893631d09d8151cee923a889
valxo <valxo@bk.ru>
2013-03-23 19:14:55 +0600

    Теперь это работает

--- game.c
+++ game.c
@@ -2,18 +2,18 @@
 #include <fcntl.h>
 
 void game(void);
-void nextCard(int, int *,int *);
+void nextCard(int*, int *,int *);
 void writing(char *, int, int);
 
 
-char answer;
+char answer[2];
 
 
 
 int main(void){
 	printf("Новая игра (N). Выход(Q).\n");
-	scanf("%c", &answer);
-	switch(answer)
+	read(0, &answer[0], 2);
+	switch(answer[0])
 	{	
 		case 'N':
 			game();
@@ -31,36 +31,39 @@ void game(){
 	int cards=0;
 	char amount[200];
 	int queue[10];
+	char que[2];
+	int i=0;
 		
 
 
-	nextCard(cards, &queue[0], &fd);
+	nextCard(&cards, &queue[0], &fd);
 
-	while(isGame){
-		nextCard(cards, &queue[0], &fd);
+	while(isGame)
+	{	
+		nextCard(&cards, &queue[0], &fd);
 		writing(&amount[0], cards, length);
-	}
-	printf("Хотите получить еще одну карту?\n");
-	scanf("%c", &answer);
-	switch(answer)
-	{
-		case 'y':
-			break;
-		case 'n':
-			isGame=0;
-			break;
-	}
+		printf("Хотите получить еще одну карту?(y/n)\n");		
+		read(0, &que[0], 2);
+		switch(que[0])
+		{
+			case 'y':
+				break;
+			case 'n':
+				isGame=0;
+				break;
+		}
+	} 
 }
 
 void writing(char *s,int printingCards, int stringEnd){
-	printf("Получено %i карт%c\n", printingCards, printingCards<4 ? 'ы': ' ');
+	printf("Получено %i карт\n", printingCards);
 	
 }
 
 
-void nextCard(int cards, int *q, int *fd){
+void nextCard(int *cards, int *q, int *fd){
 	
-	cards++;
+	(*cards)++;
 	unsigned char a;
 	int z=0;
 	read(*fd,&a,1);
@@ -68,6 +71,7 @@ void nextCard(int cards, int *q, int *fd){
 		if (*(q+z)== 0)
 		{
 			*(q+z)=(int)a;
+			z=0;
 		}
 		else ++z;
 	}
